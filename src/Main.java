40fd6e6d4b88ed87325fdbb6f951e1d0f1bb2aa2
Wadigitali <blessedtechnologies255@gmail.com>
2024-09-27 09:29:05 +0300

    Started learning Java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        //This is a dummy single line comment
+        /*This is a dummy
+        multiline
+        comment
+         */
+        //Primitives
+        int number1 = 20;
+        int number2 = 20;
+        int result = number1 +number2;
+        System.out.println(result);
+        double pi = 3.14;
+        boolean isAdult = true;
+    }
+}
